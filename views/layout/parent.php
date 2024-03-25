e3e57182f7e425f2033144a68bc93e1807dbac12
spokos <slimanekedjem0@gmail.com>
2024-03-25 10:01:34 +0100

    Modification layout

--- views/layout/parent.php
+++ views/layout/parent.php
@@ -37,7 +37,7 @@
 
     <footer class="section-p1">
         <div class="col">
-            <img class="logo" src="img/logo.png" alt="">
+            <img class="logo" img src="/phpCrud/web/assets/logo.png" alt="">
             <h4>Contact</h4>
             <p><strong>Address:</strong> DV Group of Companies,Pitam Pura,
                 Delhi-110034, India</p>
